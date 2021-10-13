cb505ee0e0f3869fb879169ca6c941e71e68a3c7
Dev-Commander <dinosmith.topdev@gmail.com>
2021-10-13 23:29:08 +0900

    modify user list and navigation and router

--- front/src/views/wbc/UserList.vue
+++ front/src/views/wbc/UserList.vue
@@ -311,14 +311,12 @@ import vSelect from 'vue-select'
 import store from '@/store'
 import { ref, onUnmounted } from '@vue/composition-api'
 import { avatarText } from '@core/utils/filter'
-import UsersListFilters from './users-list/UsersListFilters.vue'
 import useUsersList from './users-list/useUsersList'
 import userStoreModule from './users-list/userStoreModule'
 import UserListAddNew from './users-list/UserListAddNew.vue'
 
 export default {
   components: {
-    UsersListFilters,
     UserListAddNew,
     BCard,
     BRow,
